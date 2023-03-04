ba35abd105e345ba6333a60e9e868d403d2e41c6
Deepak Kumar Rai <Deepakggsipu@gmail.com>
2023-03-04 12:15:31 +0530

    update pom.xml

--- src/main/java/com/qa/report/ExtentReportListener.java
+++ src/main/java/com/qa/report/ExtentReportListener.java
@@ -1,4 +1,4 @@
-/*
+
 package com.qa.report;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -19,103 +19,105 @@ import java.util.Objects;
 
 public class ExtentReportListener extends BaseWebDriverTest implements ITestListener {
 
-  // Extent Report Declarations
-  private ExtentReports extent = ExtentManager.createInstance();
-  private static ThreadLocal<ExtentTest> extentTestThreadLocal = new ThreadLocal<>();
+    // Extent Report Declarations
+    private ExtentReports extent = ExtentManager.createInstance();
+    private static ThreadLocal<ExtentTest> extentTestThreadLocal = new ThreadLocal<>();
 
-  public static synchronized ExtentTest getExtentTest() {
-    try {
-      return extentTestThreadLocal.get();
-    } catch (Exception e) {
-      return null;
+    public static synchronized ExtentTest getExtentTest() {
+        try {
+            return extentTestThreadLocal.get();
+        } catch (Exception e) {
+            return null;
+        }
     }
-  }
 
-  @Override
-  public void onTestStart(ITestResult result) {
-    ExtentTest extentTest =
-        extent.createTest(result.getMethod().getMethodName(), result.getMethod().getDescription());
-    extentTestThreadLocal.set(extentTest);
-  }
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest =
+                extent.createTest(result.getMethod().getMethodName(), result.getMethod().getDescription());
+        extentTestThreadLocal.set(extentTest);
+    }
 
-  @Override
-  public void onTestSuccess(ITestResult result) {
-    extentTestThreadLocal.get().pass("Test Case Passed");
-  }
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        extentTestThreadLocal.get().pass("Test Case Passed");
+    }
 
-  @Override
-  public void onTestFailure(ITestResult result) {
-    Object testClass = result.getInstance();
-    WebDriver driver = ((BaseWebDriverTest) testClass).getDriver();
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Object testClass = result.getInstance();
+        WebDriver driver = ((BaseWebDriverTest) testClass).getDriver();
 
-    //        try {
-    //            test.get()
-    //                    .fail(
-    //                            "Test Case Failed",
-    //                            MediaEntityBuilder.createScreenCaptureFromPath(
-    //
-    // testUtil.takeScreenshotAtEndOfTest(result.getName(), driver))
-    //                                    .build());
-    //        } catch (IOException e) {
-    //            e.printStackTrace();
-    //        }
+        //        try {
+        //            test.get()
+        //                    .fail(
+        //                            "Test Case Failed",
+        //                            MediaEntityBuilder.createScreenCaptureFromPath(
+        //
+        // testUtil.takeScreenshotAtEndOfTest(result.getName(), driver))
+        //                                    .build());
+        //        } catch (IOException e) {
+        //            e.printStackTrace();
+        //        }
 
-    // Take base64Screenshot screenshot for extent reports
-    String base64Screenshot =
-        "data:image/png;base64,"
-            + ((TakesScreenshot) Objects.requireNonNull(driver)).getScreenshotAs(OutputType.BASE64);
-    // ExtentReports log and screenshot operations for failed tests.
-    extentTestThreadLocal
-        .get()
-        .log(
-            Status.FAIL,
-            "Test Failed",
-            extentTestThreadLocal
+        // Take base64Screenshot screenshot for extent reports
+        String base64Screenshot =
+                "data:image/png;base64,"
+                        + ((TakesScreenshot) Objects.requireNonNull(driver)).getScreenshotAs(OutputType.BASE64);
+        // ExtentReports log and screenshot operations for failed tests.
+        extentTestThreadLocal
                 .get()
-                .addScreenCaptureFromBase64String(base64Screenshot)
-                .getModel()
-                .getMedia()
-                .get(0));
+                .log(
+                        Status.FAIL,
+                        "Test Failed",
+                        extentTestThreadLocal
+                                .get()
+                                .addScreenCaptureFromBase64String(base64Screenshot)
+                                .getModel()
+                                .getMedia()
+                                .get(0));
 
-    String exceptionMessage = String.valueOf(result.getThrowable());
-    extentTestThreadLocal
-        .get()
-        .fail(
-            "<details>"
-                + "<summary>"
-                + "<b>"
-                + "<font color="
-                + "red>"
-                + "Exception Occurred:Click to see"
-                + "</font>"
-                + "</b>"
-                + "</summary>"
-                + exceptionMessage.replace(",", "<br>")
-                + "</details>"
-                + " \n");
-    // test.get().fail(result.getThrowable());
+        String exceptionMessage = String.valueOf(result.getThrowable());
+        extentTestThreadLocal
+                .get()
+                .fail(
+                        "<details>"
+                                + "<summary>"
+                                + "<b>"
+                                + "<font color="
+                                + "red>"
+                                + "Exception Occurred:Click to see"
+                                + "</font>"
+                                + "</b>"
+                                + "</summary>"
+                                + exceptionMessage.replace(",", "<br>")
+                                + "</details>"
+                                + " \n");
+        // test.get().fail(result.getThrowable());
 
-    String failureLog = "TEST CASE FAILED";
-    Markup markup = MarkupHelper.createLabel(failureLog, ExtentColor.RED);
-    extentTestThreadLocal.get().fail(markup);
-  }
+        String failureLog = "TEST CASE FAILED";
+        Markup markup = MarkupHelper.createLabel(failureLog, ExtentColor.RED);
+        extentTestThreadLocal.get().fail(markup);
+    }
 
-  @Override
-  public void onTestSkipped(ITestResult result) {
-    extentTestThreadLocal.get().skip(result.getThrowable());
-    extentTestThreadLocal.get().skip("Test Case Skipped");
-  }
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        extentTestThreadLocal.get().skip(result.getThrowable());
+        extentTestThreadLocal.get().skip("Test Case Skipped");
+    }
 
-  @Override
-  public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    }
 
-  @Override
-  public void onStart(ITestContext context) {}
+    @Override
+    public void onStart(ITestContext context) {
+    }
 
-  @Override
-  public void onFinish(ITestContext context) {
-    extent.flush();
-    extentTestThreadLocal.remove();
-  }
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+        extentTestThreadLocal.remove();
+    }
 } // End of class ExtentTestNGITestListener
-*/
+
--- src/main/java/com/qa/util/ChromeDevToolsUtil.java
+++ src/main/java/com/qa/util/ChromeDevToolsUtil.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v95.emulation.Emulation;
-import org.openqa.selenium.devtools.v95.log.Log;
-import org.openqa.selenium.devtools.v95.network.Network;
-import org.openqa.selenium.devtools.v95.network.model.ConnectionType;
-import org.openqa.selenium.devtools.v95.network.model.Headers;
-import org.openqa.selenium.devtools.v95.performance.Performance;
-import org.openqa.selenium.devtools.v95.performance.model.Metric;
+
+import org.openqa.selenium.devtools.v108.emulation.Emulation;
+import org.openqa.selenium.devtools.v108.log.Log;
+import org.openqa.selenium.devtools.v108.network.Network;
+import org.openqa.selenium.devtools.v108.network.model.ConnectionType;
+import org.openqa.selenium.devtools.v108.network.model.Headers;
+import org.openqa.selenium.devtools.v108.performance.Performance;
+import org.openqa.selenium.devtools.v108.performance.model.Metric;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.util.*;
@@ -19,221 +20,233 @@ import java.util.stream.Collectors;
 
 public class ChromeDevToolsUtil {
 
-  private DevTools devTools;
-  private WebDriver driver;
-  private String browser;
+    private DevTools devTools;
+    private WebDriver driver;
+    private String browser;
 
-  public ChromeDevToolsUtil(WebDriver driver, String browser) {
-    this.driver = driver;
-    this.browser = browser;
-    getBrowserDevTools();
-    createDevToolsSession();
-  }
-
-  /** create dev tools session */
-  public void createDevToolsSession() {
-    devTools.createSession();
-  }
-
-  /** disable dev tools network */
-  public void disableNetwork() {
-    devTools.send(Network.disable());
-  }
-
-  /** disable dev tools network */
-  private void getBrowserDevTools() {
-    switch (browser) {
-      case "chrome" -> devTools = ((ChromeDriver) driver).getDevTools();
-      case "edge" -> devTools = ((EdgeDriver) driver).getDevTools();
+    public ChromeDevToolsUtil(WebDriver driver, String browser) {
+        this.driver = driver;
+        this.browser = browser;
+        getBrowserDevTools();
+        createDevToolsSession();
     }
-  }
 
-  /** capture dev tools network http requests */
-  public void getNetworkHTTPRequest() {
-    devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
-    devTools.addListener(
-        Network.requestWillBeSent(),
-        e -> {
-          System.out.println((e.getRequest().getUrl()));
-          System.out.println(e.getRequest().getPostData());
-          System.out.println(e.getRequest().getMethod());
-        });
-  }
-
-  /**
-   * Simulating device mode
-   *
-   * @param deviceMetrics device metrics
-   */
-  public void simulateDeviceMode(Map<String, Object> deviceMetrics) {
-    switch (browser) {
-      case "chrome" -> ((ChromiumDriver) driver)
-              .executeCdpCommand("Emulation.setDeviceMetricsOverride", deviceMetrics);
-      case "edge" -> ((EdgeDriver) driver)
-              .executeCdpCommand("Emulation.setDeviceMetricsOverride", deviceMetrics);
+    /**
+     * create dev tools session
+     */
+    public void createDevToolsSession() {
+        devTools.createSession();
     }
-  }
 
-  /**
-   * Simulating Network Speed Parameters: offline * latency downloadThroughput uploadThroughput
-   * connectionType
-   *
-   * @param offline
-   * @param latency
-   * @param downloadThroughput
-   * @param uploadThroughput
-   * @param connectionType
-   */
-  public void simulateNetworkSpeed(
-      boolean offline,
-      Number latency,
-      Number downloadThroughput,
-      Number uploadThroughput,
-      String connectionType) {
-    devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
-    if (connectionType.equalsIgnoreCase("2G")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.CELLULAR2G)));
-    } else if (connectionType.equalsIgnoreCase("3G")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.CELLULAR3G)));
-    } else if (connectionType.equalsIgnoreCase("4G")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.CELLULAR4G)));
-    } else if (connectionType.equalsIgnoreCase("WIFI")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.WIFI)));
-    } else if (connectionType.equalsIgnoreCase("BLUETOOTH")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.BLUETOOTH)));
-    } else if (connectionType.equalsIgnoreCase("WIMAX")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.WIMAX)));
-    } else if (connectionType.equalsIgnoreCase("ETHERNET")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.ETHERNET)));
-    } else if (connectionType.equalsIgnoreCase("NONE")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.NONE)));
-    } else if (connectionType.equalsIgnoreCase("OTHER")) {
-      devTools.send(
-          Network.emulateNetworkConditions(
-              false,
-              latency,
-              downloadThroughput,
-              uploadThroughput,
-              Optional.of(ConnectionType.OTHER)));
+    /**
+     * disable dev tools network
+     */
+    public void disableNetwork() {
+        devTools.send(Network.disable());
     }
-    // (false, 20, 20, 50, Optional.of(ConnectionType.OTHER)));
-  }
 
-  /**
-   * set the geolocation
-   *
-   * @param latitude
-   * @param longitude
-   * @param accuracy
-   */
-  public void setGeoLocation(double latitude, double longitude, double accuracy) {
-    devTools.send(
-        Emulation.setGeolocationOverride(
-            Optional.of(latitude), Optional.of(longitude), Optional.of(accuracy)));
-  }
+    /**
+     * disable dev tools network
+     */
+    private void getBrowserDevTools() {
+        switch (browser) {
+            case "chrome" -> devTools = ((ChromeDriver) driver).getDevTools();
+            case "edge" -> devTools = ((EdgeDriver) driver).getDevTools();
+        }
+    }
 
-  /** get console logs */
-  public void getConsoleLogs() {
-    devTools.send(Log.enable());
-    devTools.addListener(
-        Log.entryAdded(),
-        logEntry -> {
-          System.out.println("log: " + logEntry.getText());
-          System.out.println("level: " + logEntry.getLevel());
-        });
-  }
+    /**
+     * capture dev tools network http requests
+     */
+    public void getNetworkHTTPRequest() {
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        devTools.addListener(
+                Network.requestWillBeSent(),
+                e -> {
+                    System.out.println((e.getRequest().getUrl()));
+                    System.out.println(e.getRequest().getPostData());
+                    System.out.println(e.getRequest().getMethod());
+                });
+    }
 
-  /** capturing performance data(metrics) */
-  public void getPerformanceMetrics() {
-    devTools.send(Performance.enable(Optional.empty()));
-    List<Metric> metrics = devTools.send(Performance.getMetrics());
-    List<String> metricNames = metrics.stream().map(o -> o.getName()).collect(Collectors.toList());
+    /**
+     * Simulating device mode
+     *
+     * @param deviceMetrics device metrics
+     */
+    public void simulateDeviceMode(Map<String, Object> deviceMetrics) {
+        switch (browser) {
+            case "chrome" -> ((ChromiumDriver) driver)
+                    .executeCdpCommand("Emulation.setDeviceMetricsOverride", deviceMetrics);
+            case "edge" -> ((EdgeDriver) driver)
+                    .executeCdpCommand("Emulation.setDeviceMetricsOverride", deviceMetrics);
+        }
+    }
 
-    devTools.send(Performance.disable());
+    /**
+     * Simulating Network Speed Parameters: offline * latency downloadThroughput uploadThroughput
+     * connectionType
+     *
+     * @param offline
+     * @param latency
+     * @param downloadThroughput
+     * @param uploadThroughput
+     * @param connectionType
+     */
+    public void simulateNetworkSpeed(
+            boolean offline,
+            Number latency,
+            Number downloadThroughput,
+            Number uploadThroughput,
+            String connectionType) {
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        if (connectionType.equalsIgnoreCase("2G")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.CELLULAR2G)));
+        } else if (connectionType.equalsIgnoreCase("3G")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.CELLULAR3G)));
+        } else if (connectionType.equalsIgnoreCase("4G")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.CELLULAR4G)));
+        } else if (connectionType.equalsIgnoreCase("WIFI")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.WIFI)));
+        } else if (connectionType.equalsIgnoreCase("BLUETOOTH")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.BLUETOOTH)));
+        } else if (connectionType.equalsIgnoreCase("WIMAX")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.WIMAX)));
+        } else if (connectionType.equalsIgnoreCase("ETHERNET")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.ETHERNET)));
+        } else if (connectionType.equalsIgnoreCase("NONE")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.NONE)));
+        } else if (connectionType.equalsIgnoreCase("OTHER")) {
+            devTools.send(
+                    Network.emulateNetworkConditions(
+                            false,
+                            latency,
+                            downloadThroughput,
+                            uploadThroughput,
+                            Optional.of(ConnectionType.OTHER)));
+        }
+        // (false, 20, 20, 50, Optional.of(ConnectionType.OTHER)));
+    }
 
-    List<String> metricsToCheck =
-        Arrays.asList(
-            "Timestamp",
-            "Documents",
-            "Frames",
-            "JSEventListeners",
-            "LayoutObjects",
-            "MediaKeySessions",
-            "Nodes",
-            "Resources",
-            "DomContentLoaded",
-            "NavigationStart");
+    /**
+     * set the geolocation
+     *
+     * @param latitude
+     * @param longitude
+     * @param accuracy
+     */
+    public void setGeoLocation(double latitude, double longitude, double accuracy) {
+        devTools.send(
+                Emulation.setGeolocationOverride(
+                        Optional.of(latitude), Optional.of(longitude), Optional.of(accuracy)));
+    }
 
-    metricsToCheck.forEach(
-        metric ->
-            System.out.println(
-                metric + " is : " + metrics.get(metricNames.indexOf(metric)).getValue()));
-  }
+    /**
+     * get console logs
+     */
+    public void getConsoleLogs() {
+        devTools.send(Log.enable());
+        devTools.addListener(
+                Log.entryAdded(),
+                logEntry -> {
+                    System.out.println("log: " + logEntry.getText());
+                    System.out.println("level: " + logEntry.getLevel());
+                });
+    }
 
-  /**
-   * login with basic auth
-   *
-   * @param username
-   * @param password
-   * @param url
-   */
-  public void loginWithBasicAuth(String username, String password, String url) {
-    devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
-    Map<String, Object> headers = new HashMap<>();
-    String basicAuth =
-        "Basic "
-            + new String(
-                new Base64().encode(String.format("%s:%s", username, password).getBytes()));
-    headers.put("Authorization", basicAuth);
-    devTools.send(Network.setExtraHTTPHeaders(new Headers(headers)));
-    driver.get(url);
-  }
+    /**
+     * capturing performance data(metrics)
+     */
+    public void getPerformanceMetrics() {
+        devTools.send(Performance.enable(Optional.empty()));
+        List<Metric> metrics = devTools.send(Performance.getMetrics());
+        List<String> metricNames = metrics.stream().map(o -> o.getName()).collect(Collectors.toList());
+
+        devTools.send(Performance.disable());
+
+        List<String> metricsToCheck =
+                Arrays.asList(
+                        "Timestamp",
+                        "Documents",
+                        "Frames",
+                        "JSEventListeners",
+                        "LayoutObjects",
+                        "MediaKeySessions",
+                        "Nodes",
+                        "Resources",
+                        "DomContentLoaded",
+                        "NavigationStart");
+
+        metricsToCheck.forEach(
+                metric ->
+                        System.out.println(
+                                metric + " is : " + metrics.get(metricNames.indexOf(metric)).getValue()));
+    }
+
+    /**
+     * login with basic auth
+     *
+     * @param username
+     * @param password
+     * @param url
+     */
+    public void loginWithBasicAuth(String username, String password, String url) {
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        Map<String, Object> headers = new HashMap<>();
+        String basicAuth =
+                "Basic "
+                        + new String(
+                        new Base64().encode(String.format("%s:%s", username, password).getBytes()));
+        headers.put("Authorization", basicAuth);
+        devTools.send(Network.setExtraHTTPHeaders(new Headers(headers)));
+        driver.get(url);
+    }
 }
